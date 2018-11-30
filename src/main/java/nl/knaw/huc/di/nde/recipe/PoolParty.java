555c440913f1b524cd3782b744c1f4dcc95a859c
Martijn Maas <martijn.maas@snelnet.net>
2018-11-30 14:36:33 +0100

    Code cleanup

--- src/main/java/nl/knaw/huc/di/nde/recipe/PoolParty.java
+++ src/main/java/nl/knaw/huc/di/nde/recipe/PoolParty.java
@@ -1,14 +1,10 @@
 package nl.knaw.huc.di.nde.recipe;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.collect.Lists;
 import net.sf.saxon.s9api.SaxonApiException;
 import net.sf.saxon.s9api.XdmItem;
 import nl.knaw.huc.di.nde.TermDTO;
 import nl.mpi.tla.util.Saxon;
-import org.apache.commons.io.IOUtils;
-import org.apache.http.HttpEntity;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpPost;
@@ -18,13 +14,11 @@ import org.apache.http.message.BasicNameValuePair;
 import org.eclipse.rdf4j.model.Model;
 import org.eclipse.rdf4j.model.Statement;
 import org.eclipse.rdf4j.rio.RDFFormat;
-import org.eclipse.rdf4j.rio.RDFParser;
 import org.eclipse.rdf4j.rio.Rio;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
-import java.io.StringWriter;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URLDecoder;
@@ -35,7 +29,6 @@ import java.util.List;
 public class PoolParty implements RecipeInterface {
 
   private static final Logger LOG = LoggerFactory.getLogger(PoolParty.class);
-  private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
 
   @Override
   public List<TermDTO> fetchMatchingTerms(XdmItem config, String match) {
@@ -53,58 +46,36 @@ public class PoolParty implements RecipeInterface {
       ArrayList<BasicNameValuePair> parameters = Lists.newArrayList(
         new BasicNameValuePair("query", query));
       post.setEntity(new UrlEncodedFormEntity(parameters));
-      // HttpGet httpGet = new HttpGet(api.toString());
       CloseableHttpResponse httpResponse = client.execute(post);
-      // String theString = entityToString(httpResponse.getEntity());
-      // LOG.info("response {}", theString);
 
       Model model = Rio.parse(httpResponse.getEntity().getContent(), "https://data.cultureelerfgoed.nl/term/id/cht/",
         RDFFormat.RDFXML);
       String subject = null;
       TermDTO term = null;
       for (Statement statement : model) {
-        if(subject == null || !statement.getSubject().stringValue().equals(subject)) {
+        if (subject == null || !statement.getSubject().stringValue().equals(subject)) {
           subject = statement.getSubject().stringValue();
-          if(term != null) {
+          if (term != null) {
             terms.add(term);
           }
           term = new TermDTO();
           term.uri = new URI(subject);
         }
-        if(statement.getPredicate().getLocalName().equals("prefLabel")) {
+        if (statement.getPredicate().getLocalName().equals("prefLabel")) {
           term.prefLabel.add(statement.getObject().stringValue());
         }
-        if(statement.getPredicate().getLocalName().equals("altLabel")) {
+        if (statement.getPredicate().getLocalName().equals("altLabel")) {
           term.altLabel.add(statement.getObject().stringValue());
         }
-        if(statement.getPredicate().getLocalName().equals("hiddenLabel")) {
+        if (statement.getPredicate().getLocalName().equals("hiddenLabel")) {
           term.definition.add(statement.getObject().stringValue());
         }
-        if(statement.getPredicate().getLocalName().equals("scopeNote")) {
+        if (statement.getPredicate().getLocalName().equals("scopeNote")) {
           term.scopeNote.add(statement.getObject().stringValue());
         }
 
       }
       terms.add(term);
-
-
-      // JsonNode jsonNode = OBJECT_MAPPER.readTree(theString);
-      // ArrayNode results = (ArrayNode) jsonNode.get("results").get("bindings");
-      // results.iterator().forEachRemaining(res -> {
-      //   TermDTO term = new TermDTO();
-      //   String uri = res.get("uri").get("value").asText();
-      //   try {
-      //     term.uri = new URI(uri);
-      //     term.definition = getOptional(res, "hiddenLabel");
-      //     term.altLabel = getOptional(res, "altLabel");
-      //     term.prefLabel = Lists.newArrayList(res.get("prefLabel").get("value").textValue());
-      //     term.scopeNote = getOptional(res, "scopeNote");
-      //   } catch (URISyntaxException e) {
-      //     LOG.error("Uri {} not an URI", uri);
-      //   }
-      //
-      //   terms.add(term);
-      // });
 
     } catch (IOException | SaxonApiException | URISyntaxException e) {
       LOG.error("Request failed: ", e);
@@ -112,18 +83,5 @@ public class PoolParty implements RecipeInterface {
 
 
     return terms;
-  }
-
-  private ArrayList<String> getOptional(JsonNode res, String field) {
-    if (res.has(field)) {
-      return Lists.newArrayList(res.get(field).get("value").asText());
-    }
-    return Lists.newArrayList();
-  }
-
-  private String entityToString(HttpEntity responseEntity) throws IOException {
-    StringWriter writer = new StringWriter();
-    IOUtils.copy(responseEntity.getContent(), writer, "UTF-8");
-    return writer.toString();
   }
 }
