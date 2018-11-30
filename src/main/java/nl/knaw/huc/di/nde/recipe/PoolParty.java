22784772c6e79e412eaff04cc893a2da8a01d471
Martijn Maas <martijn.maas@snelnet.net>
2018-11-30 14:34:32 +0100

    Make recipe work with RDF XML

--- src/main/java/nl/knaw/huc/di/nde/recipe/PoolParty.java
+++ src/main/java/nl/knaw/huc/di/nde/recipe/PoolParty.java
@@ -2,7 +2,6 @@ package nl.knaw.huc.di.nde.recipe;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.google.common.collect.Lists;
 import net.sf.saxon.s9api.SaxonApiException;
 import net.sf.saxon.s9api.XdmItem;
@@ -16,6 +15,11 @@ import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.message.BasicNameValuePair;
+import org.eclipse.rdf4j.model.Model;
+import org.eclipse.rdf4j.model.Statement;
+import org.eclipse.rdf4j.rio.RDFFormat;
+import org.eclipse.rdf4j.rio.RDFParser;
+import org.eclipse.rdf4j.rio.Rio;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -47,32 +51,62 @@ public class PoolParty implements RecipeInterface {
       CloseableHttpClient client = HttpClients.createDefault();
       HttpPost post = new HttpPost(api);
       ArrayList<BasicNameValuePair> parameters = Lists.newArrayList(
-        new BasicNameValuePair("format", "application/json"),
         new BasicNameValuePair("query", query));
       post.setEntity(new UrlEncodedFormEntity(parameters));
       // HttpGet httpGet = new HttpGet(api.toString());
       CloseableHttpResponse httpResponse = client.execute(post);
-      String theString = entityToString(httpResponse.getEntity());
+      // String theString = entityToString(httpResponse.getEntity());
       // LOG.info("response {}", theString);
-      JsonNode jsonNode = OBJECT_MAPPER.readTree(theString);
-      ArrayNode results = (ArrayNode) jsonNode.get("results").get("bindings");
-      results.iterator().forEachRemaining(res -> {
-        TermDTO term = new TermDTO();
-        String uri = res.get("uri").get("value").asText();
-        try {
-          term.uri = new URI(uri);
-          term.definition = getOptional(res, "hiddenLabel");
-          term.altLabel = getOptional(res, "altLabel");
-          term.prefLabel = Lists.newArrayList(res.get("prefLabel").get("value").textValue());
-          term.scopeNote = getOptional(res, "scopeNote");
-        } catch (URISyntaxException e) {
-          LOG.error("Uri {} not an URI", uri);
+
+      Model model = Rio.parse(httpResponse.getEntity().getContent(), "https://data.cultureelerfgoed.nl/term/id/cht/",
+        RDFFormat.RDFXML);
+      String subject = null;
+      TermDTO term = null;
+      for (Statement statement : model) {
+        if(subject == null || !statement.getSubject().stringValue().equals(subject)) {
+          subject = statement.getSubject().stringValue();
+          if(term != null) {
+            terms.add(term);
+          }
+          term = new TermDTO();
+          term.uri = new URI(subject);
+        }
+        if(statement.getPredicate().getLocalName().equals("prefLabel")) {
+          term.prefLabel.add(statement.getObject().stringValue());
+        }
+        if(statement.getPredicate().getLocalName().equals("altLabel")) {
+          term.altLabel.add(statement.getObject().stringValue());
+        }
+        if(statement.getPredicate().getLocalName().equals("hiddenLabel")) {
+          term.definition.add(statement.getObject().stringValue());
+        }
+        if(statement.getPredicate().getLocalName().equals("scopeNote")) {
+          term.scopeNote.add(statement.getObject().stringValue());
         }
 
-        terms.add(term);
-      });
+      }
+      terms.add(term);
 
-    } catch (IOException | SaxonApiException e) {
+
+      // JsonNode jsonNode = OBJECT_MAPPER.readTree(theString);
+      // ArrayNode results = (ArrayNode) jsonNode.get("results").get("bindings");
+      // results.iterator().forEachRemaining(res -> {
+      //   TermDTO term = new TermDTO();
+      //   String uri = res.get("uri").get("value").asText();
+      //   try {
+      //     term.uri = new URI(uri);
+      //     term.definition = getOptional(res, "hiddenLabel");
+      //     term.altLabel = getOptional(res, "altLabel");
+      //     term.prefLabel = Lists.newArrayList(res.get("prefLabel").get("value").textValue());
+      //     term.scopeNote = getOptional(res, "scopeNote");
+      //   } catch (URISyntaxException e) {
+      //     LOG.error("Uri {} not an URI", uri);
+      //   }
+      //
+      //   terms.add(term);
+      // });
+
+    } catch (IOException | SaxonApiException | URISyntaxException e) {
       LOG.error("Request failed: ", e);
     }
 
