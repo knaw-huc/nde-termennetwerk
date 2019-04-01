37adc28dbc0f4a34da95d34932bb17a2b9c89c87
Willem Melder <wmelder@beeldengeluid.nl>
2019-04-01 16:13:28 +0200

    fixed the changed param name for 'conceptScheme'

--- src/main/java/nl/knaw/huc/di/nde/recipe/OpenSKOS.java
+++ src/main/java/nl/knaw/huc/di/nde/recipe/OpenSKOS.java
@@ -34,14 +34,14 @@ public class OpenSKOS implements RecipeInterface {
         try {
             System.err.println("DBG: Lets cook some OpenSKOS!");
             String api = Saxon.xpath2string(config, "nde:api", null, OpenSKOS.NAMESPACES);
-            String cs  = Saxon.xpath2string(config, "nde:scheme", null, OpenSKOS.NAMESPACES);
+            String cs  = Saxon.xpath2string(config, "nde:conceptScheme", null, OpenSKOS.NAMESPACES);
             System.err.println("DBG: Ingredients:");
             System.err.println("DBG: - instance["+Saxon.xpath2string(config, "(nde:label)[1]", null, OpenSKOS.NAMESPACES)+"]");
             System.err.println("DBG: - api["+api+"]");
             System.err.println("DBG: - conceptScheme["+cs+"]");
             System.err.println("DBG: - match["+match+"]");
             // https://clavas.clarin.eu/clavas/public/api/find-concepts?q=prefLabel:*&conceptScheme=http://hdl.handle.net/11459/CLAVAS_810f8d2a-6723-3ba6-2e57-41d6d3844816&fl=uri,prefLabel&rows=100
-            URL url = new URL(api+"/find-concepts?q=prefLabel:"+match+"&conceptScheme="+cs);//+"&fl=uri,prefLabel,altLabel"
+            URL url = new URL(api+"/find-concepts?q=prefLabel:"+match+"&scheme="+cs);//+"&fl=uri,prefLabel,altLabel"
             System.err.println("DBG: = url["+url+"]");
             XdmNode res = Saxon.buildDocument(new StreamSource(url.toString()));
             for (Iterator<XdmItem> iter = Saxon.xpathIterator(res, "/rdf:RDF/rdf:Description",null, OpenSKOS.NAMESPACES); iter.hasNext();) {
