d9511b3b4c6ed0ebe2bae0352abf3c075bea203d
Enno Meijers <h.j.meijers@gmail.com>
2019-04-17 19:49:16 +0200

    added definition

--- src/main/java/nl/knaw/huc/di/nde/TermDTO.java
+++ src/main/java/nl/knaw/huc/di/nde/TermDTO.java
@@ -9,6 +9,7 @@ public class TermDTO {
   public URI uri;
   public List<String> prefLabel = Lists.newArrayList();
   public List<String> altLabel = Lists.newArrayList();
+  public List<String> hiddenLabel = Lists.newArrayList();
   public List<String> scopeNote = Lists.newArrayList();
   public List<String> definition = Lists.newArrayList();
   public List<String> broader = Lists.newArrayList();
